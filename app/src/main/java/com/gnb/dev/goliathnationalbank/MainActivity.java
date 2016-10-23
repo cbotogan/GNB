6ab38e0aeaace758c416925330e9fa05c5c4fbb2
Cristian Botogan <cristian.botogan@yahoo.com>
2016-10-23 15:02:59 +0300

    Removed unused packages

--- app/src/main/java/com/gnb/dev/goliathnationalbank/MainActivity.java
+++ app/src/main/java/com/gnb/dev/goliathnationalbank/MainActivity.java
@@ -1,31 +1,8 @@
 package com.gnb.dev.goliathnationalbank;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
